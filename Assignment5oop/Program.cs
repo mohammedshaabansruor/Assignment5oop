43eb28487d16136f7dacbb4b288af9e67d3a071d
mohammedshaabansruor <shaabanmohammed490@gmail.com>
2025-02-01 01:03:37 +0200

    Add project files.

---
+++ Assignment5oop/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment5oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
